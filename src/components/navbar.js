259067fcc8d4fe3de147f26efd277cd06aa719e6
April Cheng <april.sh.cheng@gmail.com>
2021-06-24 11:15:44 -0700

    Added navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">April Cheng</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#about-me">About Me</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#certs">Certs</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+
+            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Github</Nav.Link>
+            <Nav.Link href="#deets">LinkedIn</Nav.Link>
+
+            {/* <Nav.Link eventKey={2} href="#memes">
+              LinkedIn
+            </Nav.Link> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
