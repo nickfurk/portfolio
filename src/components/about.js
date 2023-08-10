54752ee34690bb36f05905c3adc9af63115b15a9
April Cheng <acheng118@my.bcit.ca>
2023-08-09 21:50:36 -0700

    Resolve dependency conflict

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Badge } from "react-bootstrap";
 import "../css/about.css";
-import ScrollAnimation from "react-animate-on-scroll";
 import {
   Link,
   Element,
